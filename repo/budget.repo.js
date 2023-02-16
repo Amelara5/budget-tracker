34a1cf0489bc92a4eb94a85d1fae3a48ad2ae1bf
Andres Melara <andres.melara.t@gmail.com>
2023-02-16 00:40:03 -0500

    Change budget to tracker

--- repo/budget.repo.js
+++ repo/budget.repo.js
@@ -5,20 +5,20 @@ class BudgetRepo {
 
   async getBudget() {
     // console.log("GET-REPO is good");
-    return await pool.query("SELECT * FROM budget");
+    return await pool.query("SELECT * FROM tracker");
   }
 
   async createBudget(budget) {
     // console.log("POST-REPO is good");
     return await pool.query(
-      "INSERT INTO budget (amount, type, description, date) VALUES ($1, $2, $3, $4)",
+      "INSERT INTO tracker (amount, type, description, date) VALUES ($1, $2, $3, $4)",
       [budget.amount, budget.type, budget.description, budget.date]
     );
   }
 
   async deleteBudgetLine(id) {
     // console.log("DELETE-REPO is good", id);
-    return await pool.query("DELETE FROM budget WHERE id = $1", [id]);
+    return await pool.query("DELETE FROM tracker WHERE id = $1", [id]);
   }
 }
 
